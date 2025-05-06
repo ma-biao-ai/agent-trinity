f7d349ea940b03a4d33e61ca339152a061df5794
mabiao <mabiao@utopilot.ai>
2025-05-06 16:31:09 +0800

    trinity first version add

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# agent-trinity
-Trinity is a compact agent team architecture designed to reliably complete tasks while ensuring output quality. This worker-checker-arbitrator triad overcomes the limitations of single-agent systems where errors may go undetected.
+# Trinity
+
+**Trinity** is a `compact agent team architecture` designed to reliably complete tasks while ensuring output quality. This worker-checker-arbitrator triad overcomes the limitations of single-agent systems where errors may go undetected.
+If needed, it can be extended with a restorer.worker do the input task, and checker check the result, and arbitrator arbitrate the result. If needed, restorer restore the environment.
+This architecture is developed based on the [**Agno**](https://github.com/agno-agi/agno)
+
+## Architecture
+![trinity-architecture](trinity.png)
+
---
+++ example/excel_trinity_demo.py
@@ -0,0 +1,48 @@
+from trinity import Trinity
+from textwrap import dedent
+from agno.tools.mcp import MCPTools
+from agno.models.deepseek import DeepSeek
+from agno.agent import Agent, RunResponse
+
+mcp_tools = MCPTools(
+        command=f"npx --yes @negokaz/excel-mcp-server",
+        env={"EXCEL_MCP_PAGING_CELLS_LIMIT": "4000"}
+    )
+
+excel_trinity = Trinity(
+    model=DeepSeek(id="deepseek-chat"),
+    name="excel_trinity",
+    description="A trinity for handling Excel files.",
+    mcp_tools=mcp_tools,
+    instructions=dedent("""\
+        You are an Excel operation assistant:
+        - excel file path: /home/uto/project/proj_py/agno-main/owntest/score.xlsx
+        - If the user needs to read Excel information, extract relevant data from the Excel file and present organized results;
+        - If the user requires modifications or additions, implement the requested changes to the Excel sheet;
+        - If a value is modified, check and update any affected fields as it may impact other fields.
+        - After making modifications or additions, provide a before-and-after comparison of the altered or newly added entries.\
+    """)
+)
+
+if __name__ == "__main__":
+    from rich.console import Console
+    from rich.markdown import Markdown
+    async def main():
+        console = Console()
+        while True:
+            try:
+                message = input("Enter your question: ")
+                if message.lower() in ["exit", "quit"]:
+                    break
+
+                response:RunResponse = await excel_trinity.arun(message=message)
+                if response.content:
+                    console.print(Markdown(response.content.message))
+            except Exception as e:
+                print(f"An error occurred: {e}")
+    
+    try:
+        import asyncio
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exiting...")
